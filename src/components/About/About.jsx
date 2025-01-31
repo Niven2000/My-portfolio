d0212f6397cc96021cbddcbe73e2db2ad7a6b94b
Nivethanan Sivagnanasuntharam <nivethanan2000@gmail.com>
2025-02-01 00:14:12 +0530

    My portfolio commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/MeWithLaptop.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+              I'm a beginner front-end developer passionate about creating responsive and optimized websites while continuously improving my skills.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+              I am currently exploring back-end technologies to understand server-side logic, databases, and API integration.
+              </p>
+            </div>
+          </li>
+       
+        </ul>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:nivethanan2000@gmail.com" target="_blank">nivethanan2000@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/nive-thanan-00a724342/" target="_blank">nive-thanan-00a724342</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/Niven2000" target="_blank">Niven2000</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
